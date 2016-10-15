c5d095c229eb6a5fc3fce484581c0f942e99237d
sage0316 <seiji.shii@gmail.com>
2016-10-15 20:26:36 +0000

    １回あげとくけ

--- index.php
+++ index.php
@@ -1,30 +1,16 @@
 <!DOCTYPE html>
 
 <?php
-
-	session_start();
-	
-	if (!empty($_POST)) {
-		// エラー項目の確認
-		if ($_POST['name'] == '') {
-			$error['name'] = 'blank';
-		}
+	if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
+		// ログインしている
+		// idがセッションに記録されている
+		// 最後の行動から1時間内
 		
-		if (strlen($_POST['password']) < 4) {
-			$error['password'] = 'length';
-		}
-		
-		if ($_POST['password'] == '') {
-			$error['password'] = 'blank';
-		}
-		
-		if (empty($error)) {
-			$_SESSION['join'] = $_POST;
-			header('Location: check_account.php');
-			exit();
-		}
+	} else {
+		// ログインしていない
+		header('Location: log_in/index.php');
+		exit();
 	}
-
 
 ?>
 
@@ -32,7 +18,7 @@
 	<head>
 		<link rel = 'stylesheet' href = 'style.css' type = 'text/css'>
 		<meta charset='utf-8'>
-		<title>家計簿オンライン　ログイン</title>
+		<title>家計簿オンライン</title>
 	</head>
 	
 	<body>
@@ -40,38 +26,8 @@
 			<div id = 'app_title'>
 				<h1>家計簿オンライン</h1>
 			</div>
-			<h2>ログイン</h2>
-			
-			<form action = "" method = "post" enctype = "multipart/form-data" class = 'form_column'>
-				<dl>
-					<dt>ユーザ名</dt>
-					<dd>
-						<input class='text_box' type = 'text'>
-					</dd>
-					<dt>パスワード</dt>
-					<dd>
-						<input class='text_box' type = 'password'>
-					</dd>
-				</dl>
 
-				<div class = 'right_button_wrapper'>
-					<input type="submit" value="ログイン" class = 'button'/>
-					
-					<br>
-					<br>
-					<br>
-					<div class = 'button'>
-						<a href = "sign_up/sign_up.php" class = 'link_button'>新規登録</a>					
-					</div>
-				</div>
-
-			</form>
-		
-			<br>
-			<br>
-			<br>
-			<div class = 'footer'>
-				<p>このアプリにはソーシャル機能はないためユーザ名等が公開されることはありません。</p>
+			<div class = 'footer' >
 				<p><small>&copy; 2016 c-kogyo.net</small></p>				
 			</div>
 		
